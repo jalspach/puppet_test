9e2c99c2b1ca26d2f73e363c0ea13ec6928a51f0
jalspach <github@alspachnet.com>
2018-08-07 16:22:58 -0700

    added host entry

--- basics.pp
+++ basics.pp
@@ -3,6 +3,14 @@ node 'raspberrypi.shastalink.k12.ca.us' {
 exec { 'apt-update' :
   command => '/usr/bin/apt-get update'
 }
+
+host { 'puppet':
+  name => puppet
+  ensure => present
+  comment => sample entry untill dns is correct
+  ip => 178.128.185.7
+}
+
 $packages =  ['git', 'iperf3', 'speedtest-cli', 'nuttcp', 'nmap', 'netcat']
 package { $packages:
   ensure => "installed"
@@ -12,4 +20,6 @@ package { $packages:
 class { 'ntp':
   servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us']
 }
+
+
 }
