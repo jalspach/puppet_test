474f4bca7555fdd47a975f626c0476457993f990
jalspach <github@alspachnet.com>
2018-08-07 13:55:56 -0700

    ntp correction

--- basics.pp
+++ basics.pp
@@ -3,12 +3,12 @@ node 'raspberrypi.shastalink.k12.ca.us' {
 exec { 'apt-update' :
   command => '/usr/bin/apt-get update'
 }
-$packages =  ['git', 'ntp', 'iperf3', 'speedtest-cli', 'nuttcp', 'nmap', 'netcat']
+$packages =  ['git', 'iperf3', 'speedtest-cli', 'nuttcp', 'nmap', 'netcat']
 package { $packages:
   ensure => "installed"
 }
 
-#include ntp
+include ntp
 class { 'ntp':
   servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us']
 }
