b5559541ce2e7a8bd7c4bcc86ca9c524f5c8642e
jalspach <github@alspachnet.com>
2018-08-07 13:26:42 -0700

    changes to test puppet

--- basics.pp
+++ basics.pp
@@ -3,12 +3,13 @@ node 'raspberrypi.shastalink.k12.ca.us' {
 exec { 'apt-update' :
   command => '/usr/bin/apt-get update'
 }
-user { 'jalspach':
-  ensure => present,
-  uid    => '1007',
-  gid    => '1007',
-  shell  => '/bin/bash',
-  home   => '/home/jalspach'
+$packages =  ['git', 'ntp', 'iperf3', 'speedtest-cli', 'nuttcp', 'nmap', 'netcat']
+package { $packages:
+  ensure => "installed"
 }
 
+include ntp
+class { 'ntp':
+  servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us']
+}
 }
