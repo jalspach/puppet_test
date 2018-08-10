29ce510772e4ae8a96e4982ac39f08944ec930f5
jalspach <github@alspachnet.com>
2018-08-10 12:20:31 -0700

    added refresh of test scripts via git

--- basics.pp
+++ basics.pp
@@ -21,5 +21,11 @@ class { 'ntp':
   servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us']
 }
 
+vcsrepo { '/home/pi/git/Site-Tester':
+  ensure => present,
+  provider => git,
+  source => 'git://github.com/jalspach/Site-Tester.git',
+}
 
+# add puppet configs, default ssh keys (maybe not on a public git server though), git pull of testing script, correct ntp to local ntp servers, ping matric (use puppet.db to consolidate this info.)
 }
