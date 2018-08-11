7eb04e81af910ede42f876d0082f76b9e9e82cd7
jalspach <github@alspachnet.com>
2018-08-10 17:02:17 -0700

    added some cleanup options

--- basics.pp
+++ basics.pp
@@ -4,7 +4,15 @@ exec { 'apt-update' :
   command => '/usr/bin/apt-get update'
 }
 
-host { 'host entry puppet':
+exec { 'autoremove' :
+  command => 'usr/bin/apt-get autoremove''
+}
+
+exec { 'autoclean' :
+  command => 'usr/bin/apt-get autoclean''
+}
+
+host { 'host entry puppet' :
   ensure  => 'present',
   name    => 'puppet',
   comment => 'sample entry untill dns is correct',
@@ -12,8 +20,13 @@ host { 'host entry puppet':
 }
 
 #include and configure ntp
-class { 'ntp':
+class { 'ntp' :
   servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us']
+}
+
+$packages =  ['localepurge']
+package { $packages :
+  ensure => 'installed'
 }
 
 # add puppet configs, default ssh keys (maybe not on a public git server though), git pull of testing script, correct ntp to local ntp servers, ping matric (use puppet.db to consolidate this info.)
