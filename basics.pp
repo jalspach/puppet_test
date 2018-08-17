a0f403d93590272049f876dbaaee65ff75ae91ce
jalspach <github@alspachnet.com>
2018-08-17 13:19:01 -0700

    Added crypton install

--- basics.pp
+++ basics.pp
@@ -24,9 +24,13 @@ class { 'ntp' :
   servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us']
 }
 
-$packages =  ['localepurge']
+$packages =  ['localepurge', 'curl']
 package { $packages :
   ensure => 'installed'
+}
+
+exec { 'Krypton_install' :
+  command => 'curl https://krypt.co/kr | sh'
 }
 
 # add puppet configs, default ssh keys (maybe not on a public git server though), git pull of testing script, correct ntp to local ntp servers, ping matric (use puppet.db to consolidate this info.)
