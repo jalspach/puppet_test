4fb59c537277b4126305f9d7b4bff84b5acdbf1f
jalspach <github@alspachnet.com>
2018-08-20 08:10:04 -0700

    add cron entry to auto run the tests. This will eventually fill the disk so I need to also configure logrotate and someway of offloading the data.

--- test_scripts.pp
+++ test_scripts.pp
@@ -1,9 +1,9 @@
 
 
-  $packages =  ['git', 'iperf3', 'speedtest-cli', 'nuttcp', 'nmap', 'netcat']
-  package { $packages:
-    ensure => 'installed'
-  }
+$packages =  ['git', 'iperf3', 'speedtest-cli', 'nuttcp', 'nmap', 'netcat']
+package { $packages:
+  ensure => 'installed'
+}
 
 vcsrepo { '/home/pi/git/Site-Tester/':
   ensure   => latest, #WARNING - This overwrites any local changes to the repository.
@@ -21,4 +21,10 @@ file { '/usr/local/sbin/eesd-test_server.sh':
   ensure => link,
   target => '/home/pi/git/Site-Tester/server.sh',
   mode   => '0751',
+}
+
+cron { 'throughput_testing':
+  command => '/usr/local/sbin/eesd_tests.sh',
+  user    => 'root',
+  minute  => '*/5',
 }
