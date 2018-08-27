86d29280e0194a6da5cb891efff23c651736c92a
jalspach <github@alspachnet.com>
2018-08-27 16:11:49 -0700

    fixed typo

--- basics.pp
+++ basics.pp
@@ -16,17 +16,17 @@ host { 'host entry puppet server' :
   ensure       => 'present',
   name         => 'puppet.shastalink.k12.ca.us',
   host_aliases => 'puppet',
-  comment      => 'Not necessary if name can resolve.',
+  comment      => 'Not necessary if the name can resolve.',
   ip           => '10.1.3.169',
 }
 
-include timezone
-class {'timezone':
+include timezone,
+class { 'timezone' :
   timezone => 'US/Pacific-New',
 }
 
 #include and configure ntp
-include ntp
+include ntp,
 class { 'ntp' :
   servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us'],
 }
