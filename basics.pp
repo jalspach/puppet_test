6062a9e340bf1a3cfca71b61dd3a7beac36eab4e
jalspach <github@alspachnet.com>
2018-08-27 17:09:00 -0700

    turn stuff off to test

--- basics.pp
+++ basics.pp
@@ -1,5 +1,4 @@
 node default {
-  include ntp
 
 exec { 'apt-update' :
   command => '/usr/bin/apt-get update',
@@ -13,53 +12,55 @@ exec { 'autoclean' :
   command => '/usr/bin/apt-get autoclean',
 }
 
-host { 'host entry puppet server' :
-  ensure       => 'present',
-  name         => 'puppet.shastalink.k12.ca.us',
-  host_aliases => 'puppet',
-  comment      => 'Not necessary if the name can resolve.',
-  ip           => '10.1.3.169',
-}
 
-include timezone,
-class { 'timezone' :
-  timezone => 'US/Pacific-New',
-}
 
-#include and configure ntp
-include ntp,
-class { 'ntp' :
-  servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us'],
-}
-
-$packages =  ['localepurge', 'curl', 'dnstop', 'dnsutils' ]
-package { $packages :
-  ensure => 'installed',
-}
-
-exec { 'Krypton_install' :
-  command => '/usr/bin/curl https://krypt.co/kr | sh',
-}
-
-# $nameservers = ['10.1.3.133', '10.0.128.133']
-# file { '/etc/resolv.conf':
-#   ensure => file,
-#   owner => 'root',
-#   group => 'root',
-#   mode => '0644',
-#   content => template('resolver/resolv.conf.erb')
+# host { 'host entry puppet server' :
+#   ensure       => 'present',
+#   name         => 'puppet.shastalink.k12.ca.us',
+#   host_aliases => 'puppet',
+#   comment      => 'Not necessary if the name can resolve.',
+#   ip           => '10.1.3.169',
+# }
+#
+# include timezone,
+# class { 'timezone' :
+#   timezone => 'US/Pacific-New',
+# }
+#
+# #include and configure ntp
+# include ntp,
+# class { 'ntp' :
+#   servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us'],
+# }
+#
+# $packages =  ['localepurge', 'curl', 'dnstop', 'dnsutils' ]
+# package { $packages :
+#   ensure => 'installed',
+# }
+#
+# exec { 'Krypton_install' :
+#   command => '/usr/bin/curl https://krypt.co/kr | sh',
+# }
+#
+# exec { 'Agent_autostart' :
+#   command => '/opt/puppet/bin/puppet resource service puppet ensure=running enable=true',
+# }
+#
+# include motd,
+# class { 'motd':
+#   content => 'This Pi is managed by Puppet/n',
 #   }
 
-exec { 'Agent_autostart' :
-  command => '/opt/puppet/bin/puppet resource service puppet ensure=running enable=true',
-}
-
-include motd,
-class { 'motd':
-  content => 'This Pi is managed by Puppet/n',
-  }
 
 
+  # $nameservers = ['10.1.3.133', '10.0.128.133']
+  # file { '/etc/resolv.conf':
+  #   ensure => file,
+  #   owner => 'root',
+  #   group => 'root',
+  #   mode => '0644',
+  #   content => template('resolver/resolv.conf.erb')
+  #   }
 
 # add puppet configs, default ssh keys, correct ntp to local ntp servers.
 }
