ea7708c13c1a60f07661ad2e7f7d9261da8214cb
jalspach <github@alspachnet.com>
2018-08-28 16:22:34 -0700

    turn it all back on

--- basics.pp
+++ basics.pp
@@ -21,35 +21,35 @@ host { 'host entry puppet server' :
   comment      => 'Not necessary if the name can resolve.',
   ip           => '10.1.3.169',
 }
-#
-# include timezone,
-# class { 'timezone' :
-#   timezone => 'US/Pacific-New',
-# }
-#
-# #include and configure ntp
-# include ntp,
-# class { 'ntp' :
-#   servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us'],
-# }
-#
-# $packages =  ['localepurge', 'curl', 'dnstop', 'dnsutils' ]
-# package { $packages :
-#   ensure => 'installed',
-# }
-#
-# exec { 'Krypton_install' :
-#   command => '/usr/bin/curl https://krypt.co/kr | sh',
-# }
-#
-# exec { 'Agent_autostart' :
-#   command => '/opt/puppet/bin/puppet resource service puppet ensure=running enable=true',
-# }
-#
-# include motd,
-# class { 'motd':
-#   content => 'This Pi is managed by Puppet/n',
-#   }
+
+include timezone,
+class { 'timezone' :
+  timezone => 'US/Pacific-New',
+}
+
+#include and configure ntp
+include ntp,
+class { 'ntp' :
+  servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', 'tick.shastalink.k12.ca.us','tock.shastalink.k12.ca.us'],
+}
+
+$packages =  ['localepurge', 'curl', 'dnstop', 'dnsutils' ]
+package { $packages :
+  ensure => 'installed',
+}
+
+exec { 'Krypton_install' :
+  command => '/usr/bin/curl https://krypt.co/kr | sh',
+}
+
+exec { 'Agent_autostart' :
+  command => '/opt/puppet/bin/puppet resource service puppet ensure=running enable=true',
+}
+
+include motd,
+class { 'motd':
+  content => 'This Pi is managed by Puppet/n',
+  }
 
 
 
